59f7cc1bbcec002522d7337b3169b076a75104e5
Oswaldo Linhares <oswaldolinhares@gmail.com>
2016-02-04 16:28:31 -0300

    4_Commit
    Criação da classe de domínio Ticket

--- src/main/java/com/aprendendospringboot/domain/Ticket.java
+++ src/main/java/com/aprendendospringboot/domain/Ticket.java
@@ -1,5 +1,66 @@
 package com.aprendendospringboot.domain;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+//@Entity - Anotação do JPA que marca classe como uma Entidade
+@Entity
 public class Ticket {
+	@Id
+	@GeneratedValue
+	private int code;
+	private Date creation;
+	private int priority;
+	private int description;
+	
+	@ManyToOne
+	private Account account;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public Date getCreation() {
+		return creation;
+	}
+
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public int getDescription() {
+		return description;
+	}
+
+	public void setDescription(int description) {
+		this.description = description;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
+
+	
 
 }
