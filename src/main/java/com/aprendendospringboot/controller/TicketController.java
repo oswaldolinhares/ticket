242bc28edd3297308a7fe5869e2e574f44c719ca
Oswaldo Linhares <oswaldolinhares@gmail.com>
2016-02-04 16:15:30 -0300

    3_Commit
    Refatorando e Testando Ambiente - Rode a aplicação para testar se seu
    ambiente está funcionando entrando no browser

--- src/main/java/com/aprendendospringboot/controller/TicketController.java
+++ src/main/java/com/aprendendospringboot/controller/TicketController.java
@@ -1,5 +1,17 @@
 package com.aprendendospringboot.controller;
 
-public class TicketController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+//@RestController - Anotação que indica que controlador responderá em JSON
+//@RequestMapping - Anotação que define o caminho do serviço
+@RestController
+@RequestMapping("/")
+public class TicketController {
+	//@RequestMapping - Indica que esse método é acessível pelo browser
+	@RequestMapping
+	public String teste(){
+		return "Testando Ambiente";
+	}
+	
 }
