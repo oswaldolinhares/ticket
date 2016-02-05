bd706b284ff9cf20f4273ed28e03b6fd1d44a3e5
Oswaldo Linhares <oswaldolinhares@gmail.com>
2016-02-05 10:44:38 -0300

    6_Commit
    Criando classe repository, que faz a intermediação com o banco.

--- src/main/java/com/aprendendospringboot/repository/TicketRepository.java
+++ src/main/java/com/aprendendospringboot/repository/TicketRepository.java
@@ -1,5 +1,24 @@
 package com.aprendendospringboot.repository;
 
-public class TicketRepository {
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aprendendospringboot.domain.Ticket;
+
+// Anotação que marca a classe como um repositório ela faz mediação entre a classe domínio
+// e a camada de mapeamento de dados, é passado um objeto para pesquisa e o banco é 
+// consultado e retorna uma lista de objetos semelhantes ao objeto passado
+@Repository							    	//<objeto a ser passado e id>
+public interface TicketRepository extends JpaRepository<Ticket, Integer>{ 
+	List<Ticket> findByPriority(int priority);
+	
+	List<Ticket> findByCreation(Date creation);
+	
+	List<Ticket> findByCreationAndPriority(Date creation, int priority);
+	
+	
 
 }
